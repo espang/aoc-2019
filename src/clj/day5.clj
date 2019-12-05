467d8ef78cd1d5dfd1f3fabc2e02b6163da84b41
eike.spang <eike.spang@gmail.com>
2019-12-05 10:45:35 +0000

    fix solution for part2

--- src/clj/day5.clj
+++ src/clj/day5.clj
@@ -20,10 +20,10 @@
         (op (aget arr idx1)
             (aget arr idx2))))
 
-Opcode 5 is jump-if-true: if the first parameter is non-zero, it sets the instruction pointer to the value from the second parameter. Otherwise, it does nothing.
-Opcode 6 is jump-if-false: if the first parameter is zero, it sets the instruction pointer to the value from the second parameter. Otherwise, it does nothing.
-Opcode 7 is less than: if the first parameter is less than the second parameter, it stores 1 in the position given by the third parameter. Otherwise, it stores 0.
-Opcode 8 is equals: if the first parameter is equal to the second parameter, it stores 1 in the position given by the third parameter. Otherwise, it stores 0.
+;Opcode 5 is jump-if-true: if the first parameter is non-zero, it sets the instruction pointer to the value from the second parameter. Otherwise, it does nothing.
+;Opcode 6 is jump-if-false: if the first parameter is zero, it sets the instruction pointer to the value from the second parameter. Otherwise, it does nothing.
+;Opcode 7 is less than: if the first parameter is less than the second parameter, it stores 1 in the position given by the third parameter. Otherwise, it stores 0.
+;Opcode 8 is equals: if the first parameter is equal to the second parameter, it stores 1 in the position given by the third parameter. Otherwise, it stores 0.
 
 (defn process [coll inputs] 
   (let [arr (int-array coll)]
@@ -49,10 +49,10 @@ Opcode 8 is equals: if the first parameter is equal to the second parameter, it 
                 (recur (+ idx 2) inputs))
           5 (if (zero? (aget arr first-idx))
               (recur (+ idx 3) inputs)
-              (recur second-idx inputs))
+              (recur (aget arr second-idx) inputs))
           6 (if-not (zero? (aget arr first-idx))
               (recur (+ idx 3) inputs)
-              (recur second-idx inputs))
+              (recur (aget arr second-idx) inputs))
           7 (let [to-store (if (< (aget arr first-idx)
                                   (aget arr second-idx)) 1 0)]
               (aset arr third-idx to-store)
@@ -64,9 +64,8 @@ Opcode 8 is equals: if the first parameter is equal to the second parameter, it 
           99 (into [] arr)
           (throw (Exception. (str "unexpected command " optcode " received."))))))))
 
-( < 6 7)
 ; part 1
 (process input-coll [1]) ; => 7259358
 
 ; part 2
-(process input-coll [5])
+(process input-coll [5]) ; =? 11826654
