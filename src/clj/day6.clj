cb725e0257df91e60acdbfae02c263784a22b70c
eike <eike.spang@gmail.com>
2019-12-06 08:06:23 +0000

    part1 done

--- src/clj/day6.clj
+++ src/clj/day6.clj
@@ -1,7 +1,11 @@
 (ns clj.day6)
 
-
 (def test-input ["COM)B" "B)C"])
+
+(def part1-input
+  (-> "./resources/input6.txt"
+      slurp
+      clojure.string/split-lines))
 
 (defn parse-input [input]
   (let [[inner outer] (clojure.string/split input #"\)")]
@@ -12,10 +16,22 @@
 (defn system [inputs]
   (loop [inputs inputs
          m      {}]
-    (println inputs)
     (if (empty? inputs)
       m
       (let [[inner outer] (parse-input (first inputs))]
-        (println inner)
         (recur (rest inputs)
                (update m inner (fnil conj []) outer))))))
+
+(defn orbits-of [system-map object]
+  (if-not (contains? system-map object)
+    0
+    (+ (count (system-map object))
+       (reduce +
+               (map (partial orbits-of system-map) (system-map object))))))
+
+(defn checksum [system-map]
+  (reduce +
+          (for [object (keys system-map)]
+            (orbits-of system-map object))))
+
+(checksum (system part1-input))
