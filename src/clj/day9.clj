4b89d2fd94c446c268344b5e1ef389281af089b5
eike <eike.spang@gmail.com>
2019-12-09 21:20:37 +0000

    day 9 part 1&2

--- src/clj/day9.clj
+++ src/clj/day9.clj
@@ -1,72 +1,82 @@
 (ns clj.day9)
 
-(def test-input [109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99])
-
 (def input
-  (->> "./resources/input9.txt"
-       (slurp)
-       (as-> x (clojure.string/split x #","))
+  (->> (-> "./resources/input9.txt"
+           (slurp)
+           (clojure.string/split #","))
        (map read-string)
-       (map bigint)
-       (into-array)))
+       (map bigint)))
 
-(bigint 1)
+(def extended-input 
+  (into-array (concat input (repeat 1000 0N))))
 
-(defn first-idx [arr idx v]
-  (if (odd? (quot v 100))
-    (inc idx)
-    (aget arr (inc idx))))
+(defn first-idx [{:keys [arr idx val base]}]
+  (case (mod (quot val 100) 10)
+    0 (aget arr (inc idx))
+    1 (inc idx)
+    2 (+ base (aget arr (inc idx)))))
+  
+(defn second-idx [{:keys [arr idx val base]}]
+  (case (mod (quot val 1000) 10)
+    0 (aget arr (+ 2 idx))
+    1 (+ 2 idx)
+    2 (+ base (aget arr (+ 2 idx)))))
+ 
+(defn third-idx [{:keys [arr idx val base]}]
+  (case (mod (quot val 10000) 10)
+    0 (aget arr (+ 3 idx))
+    1 (+ 3 idx)
+    2 (+ base (aget arr (+ 3 idx)))))
 
-(defn second-idx [arr idx v]
-  (if (odd? (quot v 1000))
-    (+ 2 idx)
-    (aget arr (+ 2 idx))))
+(defn add [{:keys [arr] :as state}]
+  (aset arr (third-idx state)
+        (+ (aget arr (first-idx state))
+           (aget arr (second-idx state)))))
 
-(defn third-idx [arr idx v]
-  (if (odd? (quot v 10000))
-    (+ 3 idx)
-    (aget arr (+ 3 idx))))
+(defn multi [{:keys [arr] :as state}]
+  (aset arr (third-idx state)
+        (* (aget arr (first-idx state))
+           (aget arr (second-idx state)))))
 
-(defn add [arr idx v]
-  (aset arr (third-idx arr idx v)
-        (+ (aget arr (first-idx arr idx v))
-           (aget arr (second-idx arr idx v)))))
-
-(defn multi [arr idx v]
-  (aset arr (third-idx arr idx v)
-        (* (aget arr (first-idx arr idx v))
-           (aget arr (second-idx arr idx v)))))
-
-(defn start-until-no-inputs [arr idx inputs] 
-  (loop [idx     idx
+(defn run [arr inputs] 
+  (loop [idx     0
          inputs  inputs
-         base    0]
+         base    0N]
     (let [parameter-value (aget arr idx)
-          optcode         (mod parameter-value 100)]
+          optcode         (mod parameter-value 100)
+          state           {:arr  arr
+                           :idx  idx
+                           :val  parameter-value
+                           :base base}]
       (case optcode
-        1 (do (add arr idx parameter-value)
-              (recur (+ idx 4) inputs))
-        2 (do (multi arr idx parameter-value)
-              (recur (+ idx 4) inputs))
-        3 (do (aset arr (first-idx arr idx parameter-value) (first inputs))
-              (recur (+ idx 2) (rest inputs)))
-        4 (do (println (aget arr (first-idx arr idx parameter-value)))
-              (recur (+ idx 2) inputs)) 
-        5 (if (zero? (aget arr (first-idx arr idx parameter-value)))
-            (recur (+ idx 3) inputs)
-            (recur (aget arr (second-idx arr idx parameter-value)) inputs))
-        6 (if-not (zero? (aget arr (first-idx arr idx parameter-value)))
-            (recur (+ idx 3) inputs)
-            (recur (aget arr (second-idx arr idx parameter-value)) inputs))
-        7 (let [to-store (if (< (aget arr (first-idx arr idx parameter-value))
-                                (aget arr (second-idx arr idx parameter-value))) 1 0)]
-            (aset arr (third-idx arr idx parameter-value) to-store)
-            (recur (+ idx 4) inputs))
-        8 (let [to-store (if (= (aget arr (first-idx arr idx parameter-value))
-                                (aget arr (second-idx arr idx parameter-value))) 1 0)]
-            (aset arr (third-idx arr idx parameter-value) to-store)
-            (recur (+ idx 4) inputs))
+        1 (do (add state)
+              (recur (+ idx 4) inputs base))
+        2 (do (multi state)
+              (recur (+ idx 4) inputs base))
+        3 (do (aset arr (first-idx state) (first inputs))
+              (recur (+ idx 2) (rest inputs) base))
+        4 (do (println (aget arr (first-idx state)))
+              (recur (+ idx 2) inputs base)) 
+        5 (if (zero? (aget arr (first-idx state)))
+            (recur (+ idx 3) inputs base)
+            (recur (aget arr (second-idx state)) inputs base))
+        6 (if-not (zero? (aget arr (first-idx state)))
+            (recur (+ idx 3) inputs base)
+            (recur (aget arr (second-idx state)) inputs base))
+        7 (let [to-store (if (< (aget arr (first-idx state))
+                                (aget arr (second-idx state))) 1N 0N)]
+            (aset arr (third-idx state) to-store)
+            (recur (+ idx 4) inputs base))
+        8 (let [to-store (if (= (aget arr (first-idx state))
+                                (aget arr (second-idx state))) 1N 0N)]
+            (aset arr (third-idx state) to-store)
+            (recur (+ idx 4) inputs base))
+        9 (recur (+ idx 2) inputs (+ base (aget arr (first-idx state))))
         99 (into [] arr)
         (throw (Exception. (str "unexpected command " optcode " received.")))))))
 
-(into-array [1N 2N])
+; part 1
+(run extended-input [1N])
+
+; part 2
+(run extended-input [2N])
