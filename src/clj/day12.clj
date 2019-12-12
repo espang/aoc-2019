daa0f408b79b8930e4cda0d3dbace0ec1ee0ff6b
eike <eike.spang@gmail.com>
2019-12-12 20:16:13 +0000

    simplify apply-steps

--- src/clj/day12.clj
+++ src/clj/day12.clj
@@ -1,33 +1,29 @@
 (ns clj.day12)
 
-(defn apply-step [pos vel]
+(def x-pos [13 8 -5 2])
+(def y-pos [9 14 4 -6])
+(def z-pos [5 -2 11 1])
+(def vel [0 0 0 0])
+
+(defn apply-step [[pos vel]]
   (let [v+ (for [x pos] (count (filter #(> x %) pos)))
         v- (for [x pos] (count (filter #(< x %) pos)))
         v' (mapv - vel (mapv - v+ v-))
         p' (mapv + pos v')]
     [p' v']))
 
-(defn apply-steps [pos vel n]
-  (loop [p pos
-         v vel
-         n n]
-    (if (zero? n)
-      [p v]
-      (let [[p' v'] (apply-step p v)]
-        (recur p' v' (dec n))))))
+(defn apply-steps [p v steps]
+  (nth (iterate apply-step [p v]) steps))
 
-(def x-pos [13 8 -5 2])
-(def y-pos [9 14 4 -6])
-(def z-pos [5 -2 11 1])
-(def vel [0 0 0 0])
+(defn abs [coll] (map #(Math/abs %) coll))
 
 (let [[x-pos x-vel] (apply-steps x-pos vel 1000)
       [y-pos y-vel] (apply-steps y-pos vel 1000)
       [z-pos z-vel] (apply-steps z-pos vel 1000)]
   (reduce +
           (map * 
-               (map + (map #(Math/abs %) x-pos) (map #(Math/abs %) y-pos) (map #(Math/abs %) z-pos))
-               (map + (map #(Math/abs %) x-vel) (map #(Math/abs %) y-vel) (map #(Math/abs %) z-vel)))))
+               (map + (abs x-pos) (abs y-pos) (abs z-pos))
+               (map + (abs x-vel) (abs y-vel) (abs z-vel)))))
 
 (defn period [pos vel]
   (loop [p     pos
@@ -39,18 +35,15 @@
       (let [[p' v'] (apply-step p v)]
         (recur p' v' (inc count) (conj seen [p v]))))))
 
-(period x-pos vel)
-; 268296
-(period y-pos vel)
-; 161428
-(period z-pos vel)
-; 102356
-
 (defn gcd [a b]
   (if (zero? b) a (recur b (mod a b))))
 
 (defn lcm [a b]
   (/ (* a b) (gcd a b)))
 
-(println (reduce lcm [268296 161428 102356]))
+(defn solve []
+  (let [p1 (period x-pos vel)
+        p2 (period y-pos vel)
+        p3 (period z-pos vel)]
+    (println (reduce lcm [p1 p2 p3]))))
 
